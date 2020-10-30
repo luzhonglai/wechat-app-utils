70da260c8f66565a85734c9c463691f0de7fadd2
luzhonglai <1342341673@qq.com>
2020-10-30 16:03:34 +0800

    chore: Add router function and config

--- utils/router.js
+++ utils/router.js
@@ -3,22 +3,77 @@
  * @Author: Zhonglai Lu
  * @Date: 2020-10-27 21:17:26
  * @LastEditors: Zhonglai Lu
- * @LastEditTime: 2020-10-27 22:03:19
+ * @LastEditTime: 2020-10-30 16:01:07
  */
-import { wxAPI } from './wxApi'
+import  wxAPI from './wxApi'
 
-// 配置路由key
-const pagesRouter = {
 
-}
 
-let typeArr = ['switchTap', 'naviagteTo', 'redirectTo', 'naviagteBack']
+ console.log(wxAPI)
 
-export function Router(type, options) {
-  if (!typeArr.includes(type)) {
-    new Error('输入符合的type类:'+ typeArr.json())
-    return
+/**
+ * 参数拼接
+ */ 
+function paramsJoint(path, params, isQuestion){
+  for (let item in params) {
+      path =
+          path.indexOf('?') != -1 || isQuestion
+              ? `${path}&${item}=${params[item]}`
+              : `${path}?${item}=${params[item]}`
   }
-  return wxAPI[type]({ url })
+  return path
 }
 
+
+/**
+ * 配置路由
+ */
+const pagesRouter = {
+  home: '...',
+  login:'...',
+
+}
+
+let defaultType = ['reLaunch', 'switchTab', 'redirectTo', 'navigateTo','navigateBack']
+
+/**
+ * @param {type} 调用的路由类型
+ * @param {name} 路由名
+ * @param {params} { query参数 }
+ * @param {delta} 回退页数，默认1
+ * @param {success} 成功的回调
+ * @param {fail} 失败的回调
+ * @param {complete} 不管成功或失败的回调
+ */
+export function Router(type = '', options) {
+  // 执行鉴权
+  if(options == undefined) throw new Error('没有传入 options 必须是个对象') 
+  if (options.params && options.params.constructor !== Object) throw new Error('params属性必须是一个对象')
+  if (!defaultType.includes(type)) throw new Error('输入符合的类型:' + defaultType.join())
+
+  // 处理url参数格式化
+  let url = paramsJoint(pagesRouter[options.name], options.params) || ''
+  let obj = null;
+  
+  if(type != 'navigateBack') {
+    obj = { url }
+  } else {
+    obj = {
+      delta: options.delta || 1
+    }
+  }
+  return wxAPI[type]()
+}
+
+
+
+// ---------------测试函数-------------
+// Router('redirectTo',{url:''}).then(res=>{
+//   console.log(111)
+// }).catch(error => {
+//   console.log(222)
+//   console.log(error)
+// })
+
+
+// Router('redirectTo')
