f052d1ac0ae8b4b99549d92f3ecf71f1d758285f
luzhonglai <1342341673@qq.com>
2020-11-17 15:41:29 +0800

    Add eslint Config

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: Zhonglai Lu
+ * @Date: 2020-11-17 14:36:47
+ * @LastEditors: Zhonglai Lu
+ * @LastEditTime: 2020-11-17 14:46:43
+ */
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es2020": true,
+        "es6" : true
+    },
+    "parser": "babel-eslint",
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 11
+    },
+    "rules": {
+        
+    }
+};
