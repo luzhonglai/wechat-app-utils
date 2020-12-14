93d1bfb8b1f1a66a4b9d0771104dc5a9ba55d681
ZhongLaiLu <1342341673@qq.com>
2020-12-14 17:13:43 +0800

    chore add wechat-app config

---
+++ config/index.js
@@ -0,0 +1,24 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: Zhonglai Lu
+ * @Date: 2020-12-14 16:53:33
+ * @LastEditors: Zhonglai Lu
+ * @LastEditTime: 2020-12-14 16:54:21
+ */
+
+/**
+ * 项目根配置
+ */
+module.exports = {
+  name: '小程序插件',
+  // 本地服务配置
+  server: {
+    port: 8088, // 服务的端口
+    imgPath: '/assets/wechat_app', // 静态资源的域名路径
+  },
+  // 小程序配置
+  app: {
+    fundebug: false, // 开启fundebug日志
+  },
+};
